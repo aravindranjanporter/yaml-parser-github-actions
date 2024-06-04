d9ece5d61b8f6ac3d5f4aceb84c879e77937aedd
aravindranjanporter <aravind.ranjan@theporter.in>
2024-06-04 20:54:33 +0530

    chore: initial commit to read a file

---
+++ main.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const yaml = require("js-yaml");
+
+try {
+  // Get the YAML file path from the input
+  const yamlFilePath = core.getInput("file_path");
+
+  // Read the YAML file
+  const yamlContent = fs.readFileSync(yamlFilePath, "utf8");
+
+  // Parse the YAML content
+  const parsedYaml = yaml.safeLoad(yamlContent);
+
+  console.log(parsedYaml);
+
+  // Set the output using core.setOutput
+  core.setOutput("parsed-yaml", JSON.stringify(parsedYaml));
+} catch (error) {
+  // Handle any errors
+  core.setFailed(error.message);
+}
